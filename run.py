8c18f02c6939d5b11588c6a99342301b54630ea4
xiaowu351 <xiaowu351@users.noreply.github.com>
2026-05-26 15:53:22 +0800

    flask自动部署时端口号改为默认80

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ from wxcloudrun.app import app
 
 # 启动Flask Web服务
 if __name__ == '__main__':
-    # app.run(host=sys.argv[1], port=sys.argv[2])
-    app.run(host='localhost', port=8080, debug=True)
+    app.run(host=sys.argv[1], port=sys.argv[2])
+    #app.run(host='localhost', port=8080, debug=True)
